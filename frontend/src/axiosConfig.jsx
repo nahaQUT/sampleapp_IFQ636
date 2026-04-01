b4c78de539bc0db762c2e04fb7e3958344fb18b1
rajit2709 <b00083239@aus.edu>
2026-04-01 21:53:14 +1000

    Update frontend API base URL for EC2 deployment

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.104.76.201:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
