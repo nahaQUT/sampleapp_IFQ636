54d503cf81f9d625d4c5172e006b2d311d5554b1
rajit2709 <b00083239@aus.edu>
2026-04-03 20:45:38 +1000

    Update axios config EC2  backend

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.104.76.201:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  baseURL: 'http://54.252.195.212', // local
   headers: { 'Content-Type': 'application/json' },
 });
 
