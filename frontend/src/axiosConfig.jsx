c71381a36b8c3f70dc792e4b4a4d94a86b8d711a
NithishNani072 <nithishnani072@gmail.com>
2026-04-04 22:36:44 +1000

    Update frontend base URL to EC2 public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://54.252.187.159:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
