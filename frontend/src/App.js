e386ffefb6b77a2c144371e8f66585afaf08ecef
gimhanis <gimhanis1996@gmail.com>
2026-04-05 02:21:43 +1000

    feat: added protected routes and fixes for roles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,94 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminRoute from './components/AdminRoute';
+
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
 import CustomerDashboard from './pages/CustomerDashboard';
+import CustomerOrders from './pages/CustomerOrders';
 import Cart from './pages/Cart';
-import CustomerOrders from './pages/CustomerOrders'
+
 import AdminDashboard from './pages/AdminDashboard';
 import AdminSuppliers from './pages/AdminSuppliers';
 import AdminOrders from './pages/AdminOrders';
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+
+        {/* PUBLIC ROUTES */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/CustomerDashboard" element={<CustomerDashboard />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/CustomerOrders" element={<CustomerOrders />} />
-        <Route path="/AdminDashboard" element={<AdminDashboard />} />
-        <Route path="/AdminSuppliers" element={<AdminSuppliers />} />
-        <Route path="/AdminOrders" element={<AdminOrders />} />
+
+        {/*  PROTECTED  */}
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/CustomerDashboard"
+          element={
+            <ProtectedRoute>
+              <CustomerDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/CustomerOrders"
+          element={
+            <ProtectedRoute>
+              <CustomerOrders />
+            </ProtectedRoute>
+          }
+        />
+
+        {/*  ADMIN ONLY */}
+        <Route
+          path="/AdminDashboard"
+          element={
+            <AdminRoute>
+              <AdminDashboard />
+            </AdminRoute>
+          }
+        />
+
+        <Route
+          path="/AdminSuppliers"
+          element={
+            <AdminRoute>
+              <AdminSuppliers />
+            </AdminRoute>
+          }
+        />
+
+        <Route
+          path="/AdminOrders"
+          element={
+            <AdminRoute>
+              <AdminOrders />
+            </AdminRoute>
+          }
+        />
+
       </Routes>
     </Router>
   );
