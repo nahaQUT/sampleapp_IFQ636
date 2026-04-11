43400b3b3a6f8d958f7e634a8dafcecdaf5a09e0
Atcharanan <atcharanan.siriployngam@connect.qut.edu.au>
2026-04-12 01:02:59 +1000

    Update baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://13.239.138.131/api',
+  baseURL: 'http://3.25.81.102/api',
 });
 
 axiosInstance.interceptors.request.use(
